9fb1383a77f1b814166eb590e76be0f1c86204f0
░░░░░U░Z░A░I░R░░░░ <uzairshekhani2017@gmail.com>
2024-06-26 20:56:06 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+
+app.set("view engine", "ejs")
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get("/", function(req, res){
+    res.send("welcome")
+})
+app.listen(3000)
