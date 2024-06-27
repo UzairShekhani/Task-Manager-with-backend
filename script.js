dcca2df8d0c25668d9b1895c756220548fc14303
░░░░░U░Z░A░I░R░░░░ <uzairshekhani2017@gmail.com>
2024-06-27 19:39:36 +0500

    completed

--- script.js
+++ script.js
@@ -9,7 +9,14 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, "public")))
 
 app.get("/", function(req, res){
-    fs.readdir()
-    res.render("index")
+    fs.readdir(`./files`, function(err, files) {
+        res.render("index", {files:files})
+    }) 
 }) 
+
+app.post("/create", function(req, res){
+     fs.writeFile(`./files/${req.body.title.split(" ").join('')}.txt`, req.body.details, function(err){
+        res.redirect("/")
+     });
+})
 app.listen(3000)
